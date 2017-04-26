8f9c7fb4484f55cbbc8e59aa1a329b8b7bf13a05
Jeremy Fan <jeremyfan_14@live.com>
2017-04-26 23:21:37 +0800

    fix indent

--- singleliner.py
+++ singleliner.py
@@ -3,34 +3,34 @@ import sublime_plugin
 import re
 
 class SingleLineCommand(sublime_plugin.TextCommand):
-	def run(self, edit):
-		for region in self.view.sel():
-		    if not region.empty():
-		        s = self.view.substr(region)
-		        result=[]
+  def run(self, edit):
+    for region in self.view.sel():
+      if not region.empty():
+        s = self.view.substr(region)
+        result=[]
 
-		        pattern = re.compile(r'^(.*\{)([\S\s]*)(\}[\S\s]*)$')
+        pattern = re.compile(r'^(.*\{)([\S\s]*)(\}[\S\s]*)$')
 
-		        arr=s.split('=')
+        arr=s.split('=')
 
-		        for i,a in enumerate(arr):
-		        	match = pattern.match(a)
+        for i,a in enumerate(arr):
+          match = pattern.match(a)
 
-		        	if match:
-		        		prefix = match.group(1)
-		        		obj = match.group(2)
-		        		postfix = match.group(3)
+          if match:
+            prefix = match.group(1)
+            obj = match.group(2)
+            postfix = match.group(3)
 
-		        		obj = re.sub('\s', '', obj)
+            obj = re.sub('\s', '', obj)
 
-		        		prefix = re.sub('{', '{ ', prefix)
-		        		obj = re.sub(':', ': ', obj)
-		        		obj = re.sub(',', ', ', obj)
-		        		postfix = re.sub('}', ' }', postfix)
+            prefix = re.sub('{', '{ ', prefix)
+            obj = re.sub(':', ': ', obj)
+            obj = re.sub(',', ', ', obj)
+            postfix = re.sub('}', ' }', postfix)
 
-		        		result.append(''.join([prefix, obj, postfix]))
+            result.append(''.join([prefix, obj, postfix]))
 
-		        	else:
-		        		result.append(a)
+          else:
+            result.append(a)
 
-		        self.view.replace(edit, region, '='.join(result))
+          self.view.replace(edit, region, '='.join(result))
